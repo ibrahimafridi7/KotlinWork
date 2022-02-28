5451bc5ab9c2d85574de80cd2a05d18b264c7854
Muhammad Ibrahim <ibrahimafridi152@gmail.com>
2022-02-28 16:53:41 +0500

    Initial commit

---
+++ app/src/main/java/apps/sami/kotlinbuttons/MainActivity.kt
@@ -0,0 +1,28 @@
+package apps.sami.kotlinbuttons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnOrder.setOnClickListener {
+            val checkMeatRadioButtonId = rMeat.checkedRadioButtonId
+            val meat = findViewById<RadioButton>(checkMeatRadioButtonId)
+            val kg1 = rKg1.isChecked
+            val kg2 = rKg2.isChecked
+            val kg3 = rKg3.isChecked
+            val orderString = "You Have Successfully Order:\n" +
+                    "${meat.text}" +
+                    (if (kg1) "\n 1 KG" else "" ) +
+                    (if(kg2) "\n 2 KG" else "") +
+                    (if (kg3) "\n 3 KG" else "")
+            tOrder.text = orderString
+
+        }
+    }
+}
